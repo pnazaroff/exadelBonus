4f59d06af0cb75c839947cf03865ce70838baa16
pnazaroff <78379955+pnazaroff@users.noreply.github.com>
2021-03-03 21:20:28 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: '$BACKEND'
-#  apiUrl: 'https://exadel-bonus-plus-app.herokuapp.com',
+//  apiUrl: 'https://exadel-bonus-plus-app.herokuapp.com',
 };
